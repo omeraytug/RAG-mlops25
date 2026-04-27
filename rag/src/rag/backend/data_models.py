ab93263decfe83ee427e5f7de4b9d24bb894686b
Omer Aytug <aytugomer00@gmail.com>
2026-04-27 10:50:40 +0200

    ingested data into knowledge base

--- rag/src/rag/backend/data_models.py
+++ rag/src/rag/backend/data_models.py
@@ -1,6 +1,9 @@
 from lancedb.pydantic import LanceModel, Vector
 from lancedb.embeddings import get_registry
 from rag.backend.constants import EMBEDDING_MODEL
+from dotenv import load_dotenv
+
+load_dotenv()
 
 embedding_model = get_registry().get("cohere").create(name = EMBEDDING_MODEL)
 
