64cca6e65318520a9662bdf949af92ca9f5e6f0a
Omer Aytug <aytugomer00@gmail.com>
2026-04-27 10:44:52 +0200

    ingestion:

---
+++ rag/src/rag/backend/data_models.py
@@ -0,0 +1,11 @@
+from lancedb.pydantic import LanceModel, Vector
+from lancedb.embeddings import get_registry
+from rag.backend.constants import EMBEDDING_MODEL
+
+embedding_model = get_registry().get("cohere").create(name = EMBEDDING_MODEL)
+
+class Article(LanceModel):
+    document_name: str
+    filepath: str
+    content: str = embedding_model.SourceField()
+    embedding: Vector(embedding_model.ndims()) = embedding_model.VectorField()
---
+++ rag/src/rag/setup/ingestion.py
@@ -0,0 +1,27 @@
+from rag.backend.constants import DATA_PATH, VECTOR_DB_PATH
+import lancedb
+from rag.backend.data_models import Article
+
+
+def setup_vector_db(path):
+    vector_db = lancedb.connect(uri = path)
+    vector_db.create_table("articles", schema = Article, exist_ok=True)
+
+    return vector_db
+
+def ingest_docs_to_vector_db(table):
+    for file in DATA_PATH.glob("*.txt"):
+        with open(file) as f:
+            content = f.read()
+        
+        document_name = file.name
+        # make idempotent
+        table.delete(f"document_name = '{document_name}'")
+
+        table.add([{
+            "document_name": document_name
+            "filepath": str(file)
+            "content": content
+        }])
+
+        print(table.to_pandas()["document_name"])
