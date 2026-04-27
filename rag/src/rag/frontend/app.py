bd28676ff732e27968916f55b2763965ae6a496e
Omer Aytug <aytugomer00@gmail.com>
2026-04-27 13:29:45 +0200

    frontend

---
+++ rag/src/rag/frontend/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import httpx
+
+API_URL = "http://localhost:8000"
+
+def layout():
+    st.markdown("# RAGinamls")
+    st.markdown("Ask me about tigers, fishes, rabbit, crabs and pandas")
+
+    text_input = st.text_input(label="ask a question")
+
+    if st.button("send") and text_input.strip() != "":
+        response = httpx.post(f"{API_URL}/rag/query", json={"prompt": text_input}, timeout=120)
+
+
+        data = response.json()
+        
+        st.markdown("## Question")
+        st.markdown(text_input)
+        
+        st.markdown("## Answer")
+        st.markdown(data.get("answer"))
+        
+        st.markdown("## Source")
+        st.markdown(data.get("filepath"))
+
+
+    
+if __name__ == "__main__":
+    layout()
