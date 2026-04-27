277a2739c6fb6ce2a28978167dea2d4c810df27a
Omer Aytug <aytugomer00@gmail.com>
2026-04-27 13:16:30 +0200

    implementing gemini api

--- rag/src/rag/backend/agents.py
+++ rag/src/rag/backend/agents.py
@@ -1,12 +1,12 @@
 from pydantic_ai import Agent
-from rag.backend.constants import VECTOR_DB_PATH, MODEL_LARGE
+from rag.backend.constants import VECTOR_DB_PATH, MODEL_MEDIUM
 import lancedb
 from rag.backend.data_models import RagResponse
 
 vector_db = lancedb.connect(uri=VECTOR_DB_PATH)
 
 rag_agent = Agent(
-    model=MODEL_LARGE,
+    model=MODEL_MEDIUM,
     output_type=RagResponse,
     system_prompt="""
 You are an animal expert who loves helping young pet owners (ages 10-15).
--- rag/src/rag/backend/constants.py
+++ rag/src/rag/backend/constants.py
@@ -5,6 +5,7 @@ DATA_PATH = BASE_PATH / "data"
 VECTOR_DB_PATH = BASE_PATH / 'knowledge_base'
 
 EMBEDDING_MODEL = "embed-multilingual-light-v3.0"
-MODEL_MEDIUM="openrouter:nvidia/nemotron-3-nano-30b-a3b:free"
+MODEL_SMALL="openrouter:openai/gpt-oss-20b:free"
+MODEL_MEDIUM="google-gla:gemini-3-flash-preview"
 MODEL_LARGE="openrouter:nvidia/nemotron-3-super-120b-a12b:free"
 
