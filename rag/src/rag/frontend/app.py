cd5468392977f6b3204542962a85876bf9db70bb
Ömer Aytug <aytugomer00@gmail.com>
2026-04-28 13:37:01 +0200

    chaging api url for docker and local setup

--- rag/src/rag/frontend/app.py
+++ rag/src/rag/frontend/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import httpx
+import os
 
-API_URL = "http://backend:8000"
+API_URL = os.getenv("API_URL", "http://backend:8000")
 
 def layout():
     st.markdown("# RAGinamls")
